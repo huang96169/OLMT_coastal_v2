308f49ef956645d48b183e487c28908b1df541b8
Wei Huang <huang96169@gmail.com>
2022-08-02 14:44:25 -0500

    added some files for testing col3rd

---
+++ plot_vars.m
@@ -0,0 +1,40 @@
+% This script is used for reading and processing variables from ELM outputs
+% Reading output variables
+OUTDIR='/home/whf/scratch/';
+RUNcase='RUN28';
+year=1850;
+CaseName = strcat(RUNcase,'_US-GC3_ICB20TRCNPRDCTCBC');
+f2=fopen(strcat('./H2OSFC',num2str(year),'_',RUNcase,'_vars.txt'),'wt');
+ny=year-1987+1;
+for i=year:year+1
+%for i=32:32
+   FileName = strcat(OUTDIR,CaseName,'/run/',CaseName,'.elm.h0.',num2str(i),'-01-01-00000.nc')
+
+   LEAFC = ncread(FileName,'LEAFC');
+   AGNPP = ncread(FileName,'AGNPP');
+   NPP   = ncread(FileName,'NPP');
+   TLAI  = ncread(FileName,'TLAI');
+   BGNPP = ncread(FileName,'BGNPP');
+   H2OSFC=ncread(FileName,'H2OSFC');
+   H2OSFC=H2OSFC(2,:)/1000;
+   ZWT=ncread(FileName,'ZWT');
+   %ZWT=ZWT(1,:);
+   FillData = 1.0e+36;
+   id=find(LEAFC<FillData/10);
+   LEAFC_annual(i) = sum(LEAFC(id));
+   id=find(AGNPP<FillData/10);
+   AGNPP_annual = sum(AGNPP(id))*86400
+   id=find(NPP<FillData/10);
+   NPP_annual   = sum(NPP(id))*86400
+   id=find(TLAI<FillData/10);
+   TLAI_annual(i)  = sum(TLAI(id));
+   id=find(BGNPP<FillData/10);
+   BGNPP_annual = sum(BGNPP(id))*86400
+   for j=1:length(H2OSFC)
+     fprintf(f2,'%f %10f\n',[j H2OSFC(j)]);
+   end
+end
+fclose(f2)
+%AGNPP_annual=mean(AGNPP_annual)
+%BGNPP_annual=mean(BGNPP_annual)
+%NPP_annual=mean(NPP_annual)
