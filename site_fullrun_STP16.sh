ea11ef94f936d33b7caf51a2411128d57f713935
Wei Huang <huang96169@gmail.com>
2022-09-22 09:32:08 -0500

    new file:   parm_GC4_3 	new file:   parm_GC4_4 	new file:   parm_GC4_5 	new file:   parm_GC4_6 	new file:   parm_short_GC3_11 	new file:   parm_short_GC3_12 	new file:   site_fullrun_STP15.sh 	new file:   site_fullrun_STP16.sh 	new file:   site_fullrun_STP17.sh 	new file:   site_fullrun_STP18.sh 	new file:   site_fullrun_STP19.sh 	new file:   site_fullrun_STP20.sh 	new file:   site_fullrun_STP21.sh 	new file:   site_fullrun_STP22.sh 	new file:   tidesPLM_2019_gapfill.nc

---
+++ site_fullrun_STP16.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -f
+
+python ./site_fullrun.py \
+      --site US-GC4 --site3rd US-GC3 --sitegroup Wetland --caseidprefix STP16 \
+      --nyears_ad_spinup 200 --nyears_final_spinup 600 --nyears_transient 169 --tstep 1 \
+      --cpl_bypass --machine stampede2 --compiler intel --mpilib impi \
+      --gswp3 --daymet \
+      --metdir /home1/06923/wendy611/scratch/inputdata/atm/datm7/atm_forcing.datm7.GSWP3.0.5d.v2.c180716/cpl_bypass_full \
+      --model_root /home1/06923/wendy611/scratch/E3SM_v2_WH \
+      --caseroot /home1/06923/wendy611/scratch/E3SM/cases \
+      --ccsm_input /home1/06923/wendy611/scratch/inputdata \
+      --runroot /home1/06923/wendy611/scratch/E3SM \
+      --spinup_vars \
+      --nopftdyn \
+      --col3rd \
+      --np 1 \
+      --tide_components_file /home1/06923/wendy611/OLMT_ALQUIMIA/harmonic_tides_8575512.csv \
+      --parm_file /home1/06923/wendy611/OLMT_coastal_v2/parm_GC4_3 \
+      --parm_file_2nd /home1/06923/wendy611/OLMT_coastal_v2/parm_short_GC3_12 \
