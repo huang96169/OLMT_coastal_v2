10f9aa46746d8879268231d5d97ffd78502709d8
Wei Huang <huang96169@gmail.com>
2022-09-29 09:27:16 -0500

    new file:   parm_GC4_7 	new file:   site_fullrun_STP23.sh

---
+++ site_fullrun_STP23.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -f
+
+python ./site_fullrun.py \
+      --site US-GC4 --site3rd US-GC3 --sitegroup Wetland --caseidprefix STP23 \
+      --nyears_ad_spinup 200 --nyears_final_spinup 600 --nyears_transient 169 --tstep 1 \
+      --cpl_bypass --machine stampede2 --compiler intel --mpilib impi \
+      --gswp3 --daymet \
+      --metdir /home1/06923/wendy611/scratch/inputdata/atm/datm7/atm_forcing.datm7.GSWP3.0.5d.v2.c180716/cpl_bypass_full \
+      --model_root /home1/06923/wendy611/scratch/E3SM_Salinity_3rd \
+      --caseroot /home1/06923/wendy611/scratch/E3SM/cases \
+      --ccsm_input /home1/06923/wendy611/scratch/inputdata \
+      --runroot /home1/06923/wendy611/scratch/E3SM \
+      --spinup_vars \
+      --nopftdyn \
+      --col3rd \
+      --np 1 \
+      --tide_forcing_file /home1/06923/wendy611/OLMT_coastal_v2/Annapolis_elev_sal_35yrs.nc \
+      --tide_components_file /home1/06923/wendy611/OLMT_ALQUIMIA/harmonic_tides_8575512.csv \
+      --parm_file /home1/06923/wendy611/OLMT_coastal_v2/parm_GC4_7 \
+      --parm_file_2nd /home1/06923/wendy611/OLMT_coastal_v2/parm_short_GC3_12 \
